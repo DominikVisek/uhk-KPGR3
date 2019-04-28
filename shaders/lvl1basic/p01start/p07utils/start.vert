caf4a26971aa7627e26541604573ae25df74e54d
DominikVisek <isorukujamamoto1>
2019-04-28 18:50:39 +0200

    part 1 without shadows, textures ...

--- shaders/lvl1basic/p01start/p07utils/start.vert
+++ shaders/lvl1basic/p01start/p07utils/start.vert
@@ -21,13 +21,53 @@ vec3 getSphere(vec2 xy) {
     return vec3(x, y, z);
 }
 
+vec3 getSphplot(vec2 xy) {
+    float rho = 1;
+    float phi = xy.x * PI;
+    float theta = xy.y * (2 * PI);
+
+    float x = rho * sin(phi) * cos(theta);
+    float y = rho * sin(phi) * sin(theta);
+    float z = rho * cos(phi);
+    return vec3(x, y, z);
+}
+
+vec3 getCylin(vec2 xy) {
+    float r = 1;
+    float theta = xy.y * (2 * PI);
+    float z = xy.x * 2;
+
+    float x = r * cos(theta);
+    float y = r * sin(theta);
+    return vec3(x, y, z);
+}
+
+vec3 getParsur(vec2 xy) {
+    float s = xy.y * (2 * PI);
+
+    float x = xy.x * cos(s);
+    float y = xy.x * sin(s);
+    float z = xy.x;
+
+    return vec3(x, y, z);
+}
+
 void main() {
     vec2 pos = inPosition * 2 - 1;
-	//vec2 position = inPosition;
-	//position.x += 0.1;
-	//position.y += cos(position.x + time);
-	vec3 sphere = getSphere(pos);
+    vec2 position = inPosition;
+
+    //	vec3 sphere = getSphere(pos);
+
+    position.y += sin(position.y + time);
+
+    vec3 sphere = getSphplot(pos * position.y);
+    //    vec3 sphere = getCylin(pos);
+    //    vec3 sphere = getParsur(pos);
+
+    position.x += 0.1;
+    position.y += cos(position.y + time);
     vertColor = sphere;
+
 	gl_Position = proj * view * vec4(sphere, 1.0);
 	//vertColor = inColor;
 } 
--- src/lvl1basic/p01start/p07utils/Renderer.java
+++ src/lvl1basic/p01start/p07utils/Renderer.java
@@ -1,8 +1,6 @@
 package lvl1basic.p01start.p07utils;
 
-import com.jogamp.opengl.GL2GL3;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.*;
 
 import oglutils.OGLBuffers;
 import oglutils.OGLTextRenderer;
@@ -13,6 +11,7 @@ import transforms.Mat4;
 import transforms.Mat4PerspRH;
 import transforms.Vec3D;
 
+import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseEvent;
@@ -27,173 +26,230 @@ import java.awt.event.KeyEvent;
  * Manage (create, bind, draw) vertex and index buffers using OGLBuffers class
  * in oglutils package<br/>
  * Requires JOGL 2.3.0 or newer
- * 
+ *
  * @author PGRF FIM UHK
  * @version 2.0
  * @since 2015-09-05
  */
 public class Renderer implements GLEventListener, MouseListener,
-		MouseMotionListener, KeyListener {
+        MouseMotionListener, KeyListener {
 
-	int width, height;
+    int width, height;
 
-	OGLBuffers buffers;
-	OGLTextRenderer textRenderer;
+    OGLBuffers buffers;
+    OGLTextRenderer textRenderer;
 
-	private int shaderProgram, locTime, locView, locProj;
+    private int shaderProgram, locTime, locView, locProj;
 
-	float time = 0;
-	private Mat4 proj;
-	private Camera camera;
-	private int mx, my;
+    boolean withBorder = false;
+    float time = 0;
+    private Mat4 proj;
+    private Camera camera;
+    private int mx, my;
+    private String text;
+    private Robot robot;
+    private Color color;
 
-	@Override
-	public void init(GLAutoDrawable glDrawable) {
-		// check whether shaders are supported
-		GL2GL3 gl = glDrawable.getGL().getGL2GL3();
-		OGLUtils.shaderCheck(gl);
-		
-		OGLUtils.printOGLparameters(gl);
-		
-		textRenderer = new OGLTextRenderer(gl, glDrawable.getSurfaceWidth(), glDrawable.getSurfaceHeight());
-		
-		// shader files are in /shaders/ directory
-		// shaders directory must be set as a source directory of the project
-		// e.g. in Eclipse via main menu Project/Properties/Java Build Path/Source
-		
-		shaderProgram = ShaderUtils.loadProgram(gl, "/lvl1basic/p01start/p07utils/start.vert",
-				"/lvl1basic/p01start/p07utils/start.frag",
-				null,null,null,null); 
-		
-		//shorter version of loading shader program
-		//shaderProgram = ShaderUtils.loadProgram(gl, "/lvl1basic/p01start/p04utils/start");
+    @Override
+    public void init(GLAutoDrawable glDrawable) {
+        color = new Color(0, 0, 0);
+        try {
+            this.robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
 
-		//createBuffers(gl);
-		buffers = GridFactory.generateGrid(gl, 3, 3);
+        this.text = new String("");
+        GL2GL3 gl = glDrawable.getGL().getGL2GL3();
+        OGLUtils.shaderCheck(gl);
+        OGLUtils.printOGLparameters(gl);
 
-		/*camera = new Camera(new Vec3D(5, 5, 5),
-				0, 0, 0, true);
-*/
+        textRenderer = new OGLTextRenderer(gl, glDrawable.getSurfaceWidth(), glDrawable.getSurfaceHeight());
 
-		camera = new Camera()
-				.withPosition(new Vec3D(0, 0, 0))
-				.addAzimuth(5 / 4. * Math.PI)//-3/4.
-				.addZenith(-1 / 5. * Math.PI)
-				.withFirstPerson(false)
-				.withRadius(5);
+        shaderProgram = ShaderUtils.loadProgram(gl, "/lvl1basic/p01start/p07utils/start.vert",
+                "/lvl1basic/p01start/p07utils/start.frag",
+                null, null, null, null);
 
-		locTime = gl.glGetUniformLocation(shaderProgram, "time");
-		locProj = gl.glGetUniformLocation(shaderProgram, "proj");
-		locView = gl.glGetUniformLocation(shaderProgram, "view");
-	}
+        createBuffers(gl);
+        buffers = GridFactory.generateGrid(gl, 20, 20);
 
-	void createBuffers(GL2GL3 gl) {
-		float[] vertexBufferData = {
-			-1, -1, 	0.7f, 0, 0, 
-			 1,  0,		0, 0.7f, 0,
-			 0,  1,		0, 0, 0.7f 
-		};
-		int[] indexBufferData = { 0, 1, 2 };
+        camera = new Camera()
+                .withPosition(new Vec3D(0, 0, 0))
+                .addAzimuth(5 / 4. * Math.PI)//-3/4.
+                .addZenith(-1 / 5. * Math.PI)
+                .withFirstPerson(false)
+                .withRadius(5);
 
-		// vertex binding description, concise version
-		OGLBuffers.Attrib[] attributes = {
-				new OGLBuffers.Attrib("inPosition", 2), // 2 floats
-				new OGLBuffers.Attrib("inColor", 3) // 3 floats
-		};
-		buffers = new OGLBuffers(gl, vertexBufferData, attributes, indexBufferData);
-	}
+        locTime = gl.glGetUniformLocation(shaderProgram, "time");
+        locProj = gl.glGetUniformLocation(shaderProgram, "proj");
+        locView = gl.glGetUniformLocation(shaderProgram, "view");
+    }
 
-	
-	@Override
-	public void display(GLAutoDrawable glDrawable) {
-		GL2GL3 gl = glDrawable.getGL().getGL2GL3();
-		
-		gl.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		gl.glClear(GL2GL3.GL_COLOR_BUFFER_BIT | GL2GL3.GL_DEPTH_BUFFER_BIT);
-		
-		// set the current shader to be used, could have been done only once (in
-		// init) in this sample (only one shader used)
-		gl.glUseProgram(shaderProgram); 
-		time += 0.1;
-		gl.glUniform1f(locTime, time); // correct shader must be set before this
-		gl.glUniformMatrix4fv(locView,
-				1,
-				false,
-				camera.getViewMatrix().floatArray(),
-				0
-		);
-		gl.glUniformMatrix4fv(locProj,
-				1,
-				false,
-				proj.floatArray(),
-				0
-		);
-		
-		// bind and draw
-		buffers.draw(GL2GL3.GL_TRIANGLES, shaderProgram);
-		
-		String text = new String(this.getClass().getName());
-		textRenderer.drawStr2D(3, height - 20, text);
-		textRenderer.drawStr2D(width - 90, 3, " (c) PGRF UHK");
+    void createBuffers(GL2GL3 gl) {
+        float[] vertexBufferData = {
+                -1, -1, 0.7f, 0, 0,
+                1, 0, 0, 0.7f, 0,
+                0, 1, 0, 0, 0.7f
+        };
+        int[] indexBufferData = {0, 1, 2};
 
-	}
+        OGLBuffers.Attrib[] attributes = {
+                new OGLBuffers.Attrib("inPosition", 2), // 2 floats
+                new OGLBuffers.Attrib("inColor", 3) // 3 floats
+        };
+        buffers = new OGLBuffers(gl, vertexBufferData, attributes, indexBufferData);
+    }
 
-	@Override
-	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-		this.width = width;
-		this.height = height;
-		proj = new Mat4PerspRH(Math.PI / 4, height / (double) width, 0.01, 1000.0);
-		textRenderer.updateSize(width, height);
-	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	}
+    @Override
+    public void display(GLAutoDrawable glDrawable) {
+        GL2GL3 gl = glDrawable.getGL().getGL2GL3();
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-	}
+        gl.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        gl.glClear(GL2GL3.GL_COLOR_BUFFER_BIT | GL2GL3.GL_DEPTH_BUFFER_BIT);
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-	}
+        // set the current shader to be used, could have been done only once (in
+        // init) in this sample (only one shader used)
+        gl.glUseProgram(shaderProgram);
+        time += 0.1;
+        gl.glUniform1f(locTime, time); // correct shader must be set before this
+        gl.glUniformMatrix4fv(locView,
+                1,
+                false,
+                camera.getViewMatrix().floatArray(),
+                0
+        );
+        gl.glUniformMatrix4fv(locProj,
+                1,
+                false,
+                proj.floatArray(),
+                0
+        );
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-	}
+        gl.glUniform1f(locTime, time); // correct shader must be set before this
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	}
+        if (withBorder) {
+            gl.glPolygonMode(GL2GL3.GL_FRONT_AND_BACK, GL2GL3.GL_LINE);
+        } else {
+            gl.glPolygonMode(GL2GL3.GL_FRONT_AND_BACK, GL2GL3.GL_FILL);
+        }
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		camera = camera.addAzimuth(Math.PI * (mx - e.getX()) / width);
-		camera = camera.addZenith(Math.PI * (e.getY() - my) / width);
-		mx = e.getX();
-		my = e.getY();
-	}
+        // bind and draw
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-	}
+        // buffers.draw(GL2GL3.GL_TRIANGLE_STRIP, shaderProgram);
+        buffers.draw(GL2GL3.GL_TRIANGLES, shaderProgram);
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-	}
+        textRenderer.drawStr2D(3, height - 20, text);
+        textRenderer.drawStr2D(width - 90, 3, " (c) PGRF UHK");
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
+        final GL2 glColorRectangle = glDrawable.getGL().getGL2();
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
+        glColorRectangle.glBegin(GL2.GL_POLYGON);
 
-	@Override
-	public void dispose(GLAutoDrawable glDrawable) {
-		GL2GL3 gl = glDrawable.getGL().getGL2GL3();
-		gl.glDeleteProgram(shaderProgram);
-	}
+        glColorRectangle.glColor4f((float) color.getRed() / 255, (float) (color.getGreen() / 255), (float) color.getBlue() / 255, (float) color.getAlpha() / 255);
+        glColorRectangle.glVertex2f(0.5f, 0.5f);
+        glColorRectangle.glVertex2f(1.0f, 0.5f);
+        glColorRectangle.glVertex2f(1.0f, 1.0f);
+        glColorRectangle.glVertex2f(0.5f, 1.0f);
+
+        glColorRectangle.glEnd();
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        this.width = width;
+        this.height = height;
+        proj = new Mat4PerspRH(Math.PI / 4, height / (double) width, 0.01, 1000.0);
+        textRenderer.updateSize(width, height);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        camera = camera.addAzimuth(Math.PI * (mx - e.getX()) / width);
+        camera = camera.addZenith(Math.PI * (e.getY() - my) / width);
+        mx = e.getX();
+        my = e.getY();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        color = robot.getPixelColor(e.getX(), e.getY());
+        String colorText = "Red: " + color.getRed() +
+                "Green: " + color.getGreen() +
+                "Blue: " + color.getBlue() +
+                "Alpha: " + color.getAlpha();
+
+        this.text = "X:" + String.valueOf(e.getX()) + ", Y: " + String.valueOf(e.getY()) + ", barva: " + colorText;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                camera = camera.forward(1);
+                break;
+            case KeyEvent.VK_D:
+                camera = camera.right(1);
+                break;
+            case KeyEvent.VK_S:
+                camera = camera.backward(1);
+                break;
+            case KeyEvent.VK_A:
+                camera = camera.left(1);
+                break;
+            case KeyEvent.VK_CONTROL:
+                camera = camera.down(1);
+                break;
+            case KeyEvent.VK_SHIFT:
+                camera = camera.up(1);
+                break;
+            case KeyEvent.VK_SPACE:
+                camera = camera.withFirstPerson(!camera.getFirstPerson());
+                break;
+            case KeyEvent.VK_R:
+                camera = camera.mulRadius(0.9f);
+                break;
+            case KeyEvent.VK_F:
+                camera = camera.mulRadius(1.1f);
+                break;
+            case KeyEvent.VK_B:
+                withBorder = !withBorder;
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glDrawable) {
+        GL2GL3 gl = glDrawable.getGL().getGL2GL3();
+        gl.glDeleteProgram(shaderProgram);
+    }
 
 }
